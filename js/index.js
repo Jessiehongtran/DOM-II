89c4cdea89c00bfde5bc66056b72ca9147c150eb
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-18 18:13:21 -0400

    created mouseover for navlink

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+const navLinks = document.getElementsByTagName("a");
+
+const navcolor = (i)=>{
+    navLinks[i].addEventListener('mouseover',(event)=>{
+    event.target.style.color = 'orange';})
+};
+navcolor(0);
+navcolor(1);
+navcolor(2);
+navcolor(3);
+
