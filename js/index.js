7c69cbed7dafe8cb622114e42e5e5d83b14ae681
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-18 18:56:51 -0400

    figured out to use querySelector instead of getElementByClassName and the index

--- js/index.js
+++ js/index.js
@@ -12,12 +12,21 @@ navcolor(1);
 navcolor(2);
 navcolor(3);
 
-const logo = document.getElementsByClassName("logo-heading");
-logo[0].addEventListener("click",function(){
-    if(logo[0].textContent==="Fun Bus"){
-        logo[0].textContent = "Cute Bus!";
+const logo = document.querySelector(".logo-heading");
+// logo[0].addEventListener("click",function(){
+//     if(logo[0].textContent==="Fun Bus"){
+//         logo[0].textContent = "Cute Bus!";
+//     }
+//     else {
+//         logo[0].textContent = "Fun Bus"
+//     }
+// });
+
+logo.addEventListener("click",function(){
+    if(logo.textContent==="Fun Bus"){
+    logo.textContent = "Cute Bus!";
     }
     else {
-        logo[0].textContent = "Fun Bus"
+    logo.textContent = "Fun Bus"
     }
-});
+    });
